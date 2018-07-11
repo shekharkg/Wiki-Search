6d330602f954dc4353dd3e9ddadc6d0a8966a9c6
Shekhar Gupta <shekhar@tricog.com>
2018-07-12 02:34:05 +0530

    updated broken image

--- app/src/main/java/com/shekharkg/wikisearch/viewholders/SearchResultViewHolder.kt
+++ app/src/main/java/com/shekharkg/wikisearch/viewholders/SearchResultViewHolder.kt
@@ -3,7 +3,6 @@ package com.shekharkg.wikisearch.viewholders
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import com.shekharkg.wikisearch.R
-
 import com.shekharkg.wikisearch.dao.Page
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_search_result.view.*
@@ -16,18 +15,23 @@ class SearchResultViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
     itemView.titleTV.text = page.title
     itemView.descTV.text = page.description
 
-    itemView.imageLoader.visibility = View.VISIBLE
-    Picasso.get().load(page.thumbnailUrl)
-        .placeholder(R.drawable.white_image)
-        .into(itemView.resultIV, object : com.squareup.picasso.Callback {
-          override fun onSuccess() {
-            itemView.imageLoader.visibility = View.GONE
-          }
+    if (page.thumbnailUrl != null) {
+      itemView.imageLoader.visibility = View.VISIBLE
+      Picasso.get().load(page.thumbnailUrl)
+          .placeholder(R.drawable.white_image)
+          .into(itemView.resultIV, object : com.squareup.picasso.Callback {
+            override fun onSuccess() {
+              itemView.imageLoader.visibility = View.GONE
+            }
 
-          override fun onError(ex: Exception) {
-            itemView.imageLoader.visibility = View.GONE
-            itemView.resultIV.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_broken_image))
-          }
-        })
+            override fun onError(ex: Exception) {
+              itemView.imageLoader.visibility = View.GONE
+              itemView.resultIV.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_broken_image))
+            }
+          })
+    } else {
+      itemView.imageLoader.visibility = View.GONE
+      itemView.resultIV.setImageDrawable(itemView.context.getDrawable(R.drawable.ic_broken_image))
+    }
   }
 }
